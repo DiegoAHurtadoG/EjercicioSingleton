fdfee427a432385b7e65e0cffd64b63a66bfef96
Diego Hurtado <dhurtado@altioracorp.com>
2021-03-10 11:22:19 -0500

    Creación del ejercicio usando Singleton

---
+++ PatronDiseño/PatronDiseño/Models/CatalogoAnio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatronDiseño.Models
+{
+    public class CatalogoAnio
+    {   
+        private static CatalogoAnio instancia = null;
+        public string anioAct = "";
+        public string anioAct1 = "";
+        public string anioAct2 = "";
+
+        public CatalogoAnio() {
+            anioAct = "2021";
+            anioAct1 = "2020";
+            anioAct2 = "2019";
+        }
+
+        public static CatalogoAnio Instanciar {
+            get {
+                if (instancia == null) {
+                    instancia = new CatalogoAnio();
+                }
+                return instancia;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EjercicioSingleton
 Uso de patrón de diseño Singleton
+
+El caso practico demuestra Singleton, un patron de diseño para crear objetos
+que asegura que solo va a existir un objeto de la clase
+y es utilizado para acceder a un recurso una sola vez
+la primera vez se construira pero las siguientes veces ya no se construira 
+entonces solo retornara
+Un uso practico seria la intancia de Catalogos
